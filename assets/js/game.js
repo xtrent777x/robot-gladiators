bf92af1d2fd52070017b16adcded6fa48f40ed03
Trent Roesbery <xtrent777x@gmail.com>
2021-06-26 09:04:25 -0600

    debugger

--- assets/js/game.js
+++ assets/js/game.js
@@ -7,17 +7,18 @@ var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
+var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
 
 var fight = function(enemyName) { 
         
 // Alert players that they are starting the round
-window.alert("Welcome to Robot Gladiators!");
-
-var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
 
 
-// if player choses to fight, then fight
+// repeat and execute as long as the enemy-robot is alive 
+while(enemyHealth > 0) {
+    // place fight function code block here . . .
+ // if player choses to fight, then fight
 if (promptFight === "fight" || promptFight === "FIGHT") {
     // remove enemy's health by subtracting the amount set in the playerAttack variable
     enemyHealth = enemyHealth - playerAttack;
@@ -61,10 +62,16 @@ if (promptFight === "fight" || promptFight === "FIGHT") {
     }
   }
 
+
+}
+
+
 };
 
-for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
-  }
+for (var i = 0; i < enemyNames.length; i++) {
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
+}
     
    
