73e42f2e478bb068d9adf257494c5cb58093b784
Trent Roesbery <xtrent777x@gmail.com>
2021-06-27 16:41:15 -0600

    name

--- assets/js/game.js
+++ assets/js/game.js
@@ -155,8 +155,21 @@ else {
     return value;
   };
 
+  // function to set name
+var getPlayerName = function() {
+    var name = "";
+  
+    while (name === "" || name === null) {
+        name = prompt("What is your robot's name?");
+      }
+  
+    console.log("Your robot's name is " + name);
+    return name;
+  };
+
+
   var playerInfo = {
-    name: window.prompt("What is your robot's name?"),
+    name: getPlayerName(),
     health: 100,
     attack: 10,
     money: 10,
